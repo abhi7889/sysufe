a58e3f6751743db0a26b8be2e25192ce8ca5cb51
abhi7889 <abhishek131214@gmail.com>
2024-03-11 00:17:02 +0530

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Navbar from "./Navbar/Navbar"
+import Popular from "./Popular/Popular"
+import Story from "./Story/Story"
+
+function App(){
+  return (
+    <div className="header">
+      <Navbar/>
+      {/* <Popular /> */}
+      <Story />
+    </div>
+  )
+}
+
+export default App
+
+
+
